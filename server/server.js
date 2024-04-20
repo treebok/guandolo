0b6e475659461573427e9c6c826f0b46c003a9bd
treebok <juanhenaob@gmail.com>
2024-04-20 15:32:45 -0500

    set temperature to 0.7 to model gpt-3.5-turbo-1106
    Update server.js set temperature to 0.7 to model gpt-3.5-turbo-1106

--- server/server.js
+++ server/server.js
@@ -94,7 +94,7 @@ app.post('/', async (req, res) => {
       const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo-1106",
         messages: prompt,
-        temperature: 0,
+        temperature: 0.7,
         response_format: {"type": "json_object"},
       });
       res.status(200).send({
